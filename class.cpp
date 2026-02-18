1723c6a46cd0ce1b0a174ef7fcdb7c9c19c8a234
Muhammad Bilal <mb16837157@gmail.com>
2026-02-18 22:05:38 +0500

    your message

--- class.cpp
+++ class.cpp
@@ -1,9 +1,9 @@
-#include<iostream>
+// #include<iostream>
 
-int main(){
-   std :: cout<< "Hello World";
-   return 0;
-}
+// int main(){
+//    std :: cout<< "Hello World";
+//    return 0;
+// }
 #include <iostream>
 using namespace std;
 
