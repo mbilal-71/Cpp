9ff6ff776831a2946568af98cb994fe8b27d258f
Muhammad Bilal <m.bilalferoz1@gmail.com>
2026-09-22 15:36:17 +0500

    c++

--- class.cpp
+++ class.cpp
@@ -1,13 +1,7 @@
-// #include<iostream>
-
-// int main(){
-//    std :: cout<< "Hello World";
-//    return 0;
-// }
 #include <iostream>
 using namespace std;
 
-
-
+int main(){
+    cout << "Hello World";
     return 0;
 }
