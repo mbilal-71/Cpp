a210a6e9f3c032bfec57dd0f31dbd6c9212866dd
Muhammad Bilal <mb16837157@gmail.com>
2026-09-24 01:56:41 +0500

    c++

--- class.cpp
+++ class.cpp
@@ -1,12 +1,56 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int  a = 6;
-    int  b = 16;
-    float pi = 3.14;
-    cout << "The value of a is " << a<< ". The value of b is " << b;
-    cout << "\nThe value of pi is : " <<pi;
-    return 0;
-}
+// 1.[VARIABLES]:
 
+    //   int c = 45;
+
+// int main(){
+//     int  a = 6, b=16, c=26;
+//     float pi = 3.14;
+//     cout << "The value of a is " << a<< ". The value of b is " << b <<". The value of c is " << c;
+//     cout << "\nThe value of pi is : " <<pi;
+//     cout << "\nThe value of global c is : " << ::c;  // :: key
+//     return 0;
+// }
+
+// 2.[INPUT / OUTPUT]:
+
+// int main()
+// {
+//    int num1, num2;
+
+//    cout << "Enter the value of num1: \n"; // << insertion operator
+//    cin >> num1; // >> extraction operator
+
+//    cout << "Enter the value of num2: \n";  
+//    cin >> num2; 
+
+//    cout << "The sum of num1 + num2 is: " << num1+num2;
+//     return 0;
+// }
+
+// 3.[HEADER FILES AND OPERATORS]:  
+
+// system header files: it comes with compiler #include <iostream> 
+// user defined: written by programmer
+
+// int main()
+// {   //[Arithmetic operators]:
+
+//     //running in flow following one after each
+//     int a=4,b=5;
+//     // cout << "The value of a++ is: "<<a++  << endl; // phly simple a print ho ga bd ma addition ho gi
+//     // cout << "The value of a   is: "<< a   << endl; // ab a ki value upr waly increment ki wja sa 5 ho chuki
+//     // cout << "The value of a-- is: "<< a-- << endl; // again phly a print ho ga jiski value 5 hwi thi in previous step phr usky bd decrement ho ga 
+//     // cout << "The value of a   is: "<< a   << endl; // ab a ki value upr waly decrement ki wja sa 4 ho chuki
+//     // cout << "The value of ++a is: "<< ++a << endl; // phly increment ho ga previous value ma then a print ho ga 
+//     // cout << "The value of a   is: "<< a   << endl;
+//     // cout << "The value of --a is: "<< --a << endl;
+//     // cout << "The value of a   is: "<< a   << endl;
+    
+//     //[Assignment operators]:
+    
+//    cout << (a == b);
+//     return 0;
+// }
