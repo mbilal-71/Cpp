771e8d3419a5859fc2c0d5dcedc11b7ded5bc4f2
Muhammad Bilal <mb16837157@gmail.com>
2026-09-23 16:35:46 +0500

    c++

--- class.cpp
+++ class.cpp
@@ -2,7 +2,11 @@
 using namespace std;
 
 int main(){
-    int sum = 6;
-    cout << "Hello World" << sum;
+    int  a = 6;
+    int  b = 16;
+    float pi = 3.14;
+    cout << "The value of a is " << a<< ". The value of b is " << b;
+    cout << "\nThe value of pi is : " <<pi;
     return 0;
 }
+
