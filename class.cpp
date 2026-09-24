e88c47571e5af502db7bea190860db72828d3d8b
Muhammad Bilal <mb16837157@gmail.com>
2026-09-24 20:55:25 +0500

    c++

--- class.cpp
+++ class.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 // 1.[VARIABLES]:
@@ -16,8 +17,8 @@ using namespace std;
 
 // 2.[INPUT / OUTPUT]:
 
-// int main()
-// {
+int main()
+{
 //    int num1, num2;
 
 //    cout << "Enter the value of num1: \n"; // << insertion operator
@@ -35,8 +36,8 @@ using namespace std;
 // system header files: it comes with compiler #include <iostream> 
 // user defined: written by programmer
 
-// int main()
-// {   //[Arithmetic operators]:
+// 4.[Arithmetic operators]:
+
 
 //     //running in flow following one after each
 //     int a=4,b=5;
@@ -49,8 +50,66 @@ using namespace std;
 //     // cout << "The value of --a is: "<< --a << endl;
 //     // cout << "The value of a   is: "<< a   << endl;
     
-//     //[Assignment operators]:
+// 4.[Assignment operators]:
     
 //    cout << (a == b);
-//     return 0;
-// }
+
+// 5.[Float, Double, longDouble Literals]:
+
+// float d = 34.4f;
+// double e = 34.4;
+// long double f = 34.4L;
+
+// cout << "The value of d is: " <<d <<endl <<"The value of e is: " <<e <<endl;
+// cout << "The size of d (float) is: " << sizeof(d) <<endl <<"The size of e (double) is: " << sizeof(e) <<endl <<"The size of f (longDouble) is: " << sizeof(f) <<endl;
+//   cout <<"The size of 34.4 (basically float but by default taken as double in c++) is: " << sizeof(34.4);
+
+// 6.[Reference Variables]:
+
+// float x = 455;
+// float & y = x;
+
+// cout << x << endl; //output 455
+// cout << y; //output 455
+
+// 7.[TypeCasting]:
+
+//    int a = 45;
+//    float b= 45.46;
+//    int c = int(b);
+
+//    cout << "The value of a is: " << float(a) <<endl; // int converted into float
+//    cout << "The value of b is: " << int(b) <<endl; // float converted into int
+//    cout << c <<endl;
+//    cout << a+b <<endl;
+//    cout << a+ int(b) <<endl;
+
+
+// 8.[Constants]; 
+
+    //   const int a = 35;
+    //   int a = 45; // will give error bcz it is already decleared
+    //   cout << a;
+
+// 9.[Manipulators];
+     
+   // <<endl; is manipulator
+   // setw coming from #include <iomanip> is manipulator
+
+    // int a=6, b=31,c=9087;
+
+    // cout << "The value of a before setw is: " <<a <<endl;
+    // cout << "The value of b before setw is: " <<b <<endl;
+    // cout << "The value of c before setw is: " <<c <<endl;
+    // cout << "The value of c after setw is : " <<setw(4) <<c <<endl; // sets width upto 4 digits. either it is one digit but it will take place of 4 digits by creating space in front
+    // cout << "The value of a after setw is : " <<setw(4) <<a <<endl; 
+    // cout << "The value of b after setw is : " <<setw(4) <<b <<endl;
+
+// 10.[Operator Precedence]:
+
+    // int a = 3, b=5,
+    // c = ((((a*5) + b)-45)+87);
+    // cout << c;
+
+    return 0;
+}
